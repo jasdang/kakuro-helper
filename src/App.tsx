5401781fc1b8fb3629ffe5a24ad4fad8e263ec44
jasmined09 <jasmined@anafore.com>
2021-08-21 15:28:54 +0800

    add simple ui

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,27 @@ import './App.css'
 const App: React.FC = () => {
   return (
     <div className="App">
-      <h1>WELCOME</h1>
+      <h1>KAKURO HELPER</h1>
+      <h3>NUMBERS TO SUM</h3>
+      <form action="">
+        <button type="button">1</button>
+        <button type="button">2</button>
+        <button type="button">3</button>
+        <button type="button">4</button>
+        <button type="button">5</button>
+        <button type="button">6</button>
+        <button type="button">7</button>
+        <button type="button">8</button>
+        <button type="button">9</button>
+        <button type="submit">Submit</button>
+      </form>
+      <br />
+      <h3>BREAK SUM TO NUMBERS</h3>
+      <form action="">
+        <input type="number" placeholder="Sum" />
+        <input type="number" placeholder="Nos of digits" />
+        <button type="submit">Submit</button>
+      </form>
     </div>
   )
 }
