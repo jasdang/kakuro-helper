170243a6f6ff77fe6018183a177b2610e67ca4c3
jasmined09 <jasmined@anafore.com>
2021-08-22 14:56:18 +0800

    replace form with NumbersToSum

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,12 @@
 import React from 'react'
 import './App.css'
+import NumbersToSum from './components/NumbersToSum'
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <h1>KAKURO HELPER</h1>
-      <h3>NUMBERS TO SUM</h3>
-      <form action="">
-        <button type="button">1</button>
-        <button type="button">2</button>
-        <button type="button">3</button>
-        <button type="button">4</button>
-        <button type="button">5</button>
-        <button type="button">6</button>
-        <button type="button">7</button>
-        <button type="button">8</button>
-        <button type="button">9</button>
-        <button type="submit">Submit</button>
-      </form>
+      <NumbersToSum />
       <br />
       <h3>BREAK SUM TO NUMBERS</h3>
       <form action="">
