ac869d3fa5a2fdce40074004acc691d42a3951ab
jasmined09 <jasmined@anafore.com>
2021-08-22 21:24:22 +0800

    add state sum and numOfDigits

--- src/components/SumToNumbers.tsx
+++ src/components/SumToNumbers.tsx
@@ -1,12 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const SumToNumbers = (): JSX.Element => {
+  const [sum, setSum] = useState(0)
+  const [numOfDigits, setNumOfDigits] = useState(0)
+  const handleFormSubmit = (e: any) => {
+    e.preventDefault()
+    // TODO: Break sum to numbers
+  }
   return (
     <span>
       <h3>BREAK SUM TO NUMBERS</h3>
-      <form action="">
-        <input type="number" placeholder="Sum" />
-        <input type="number" placeholder="Nos of digits" />
+      <form onSubmit={handleFormSubmit}>
+        <input
+          type="number"
+          placeholder="Sum"
+          value={sum}
+          onChange={(e: any) => setSum(parseInt(e.target.value, 10))}
+        />
+        <input
+          type="number"
+          placeholder="Nos of digits"
+          value={numOfDigits}
+          onChange={(e: any) => setNumOfDigits(parseInt(e.target.value, 10))}
+        />
         <button type="submit">Submit</button>
       </form>
     </span>
