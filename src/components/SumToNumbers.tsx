55e790382cf270185bf447df4b1453c0fa7390d7
jasmined09 <jasmined@anafore.com>
2021-08-28 16:38:24 +0800

    get combinations

--- src/components/SumToNumbers.tsx
+++ src/components/SumToNumbers.tsx
@@ -5,8 +5,56 @@ type CombinationStore = {
   [index: number]: Combination[]
 }
 
+const getCombinationFromArray = (
+  numOfDigits: number,
+  arr: number[]
+): Combination[] => {
+  if (arr.length === 0) {
+    return []
+  }
+  if (numOfDigits > arr.length) {
+    return []
+  }
+  if (numOfDigits < 1) {
+    return []
+  }
+  if (numOfDigits === 1) {
+    return arr.map((el) => [el])
+  }
+  const results = []
+
+  for (let id = 0; id < arr.length; id += 1) {
+    const firstNum = arr[id]
+    const subArr = [...arr]
+    subArr.splice(0, id + 1)
+    const remainNums = getCombinationFromArray(numOfDigits - 1, subArr)
+    const subRes = remainNums.map((pair) => [firstNum, ...pair])
+    results.push(...subRes)
+  }
+  return results
+}
+const getCombinationOfAllLengthFrom1To9 = (): Combination[] => {
+  const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+  const result: Combination[] = []
+  for (let num = 1; num <= 9; num += 1) {
+    const combs = getCombinationFromArray(num, numbers)
+    result.push(...combs)
+  }
+  return result
+}
 const getCombinationHash = (): CombinationStore => {
-  return {}
+  const combinationHash: CombinationStore = {}
+  const combinations = getCombinationOfAllLengthFrom1To9()
+  for (const comb of combinations) {
+    const sum = comb.reduce((acc, val) => acc + val, 0)
+    if (combinationHash[sum]) {
+      combinationHash[sum].push(comb)
+    } else {
+      combinationHash[sum] = [comb]
+    }
+  }
+  console.log(combinationHash)
+  return combinationHash
 }
 
 const SumToNumbers = (): JSX.Element => {
@@ -22,6 +70,7 @@ const SumToNumbers = (): JSX.Element => {
   useEffect(() => {
     // TODO: set CombinationStore
     const combinationHash = getCombinationHash()
+    console.log(combinationHash)
     setCombinationStore(combinationHash)
     return setCombinationStore({})
   }, [])
