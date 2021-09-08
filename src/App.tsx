6fe1727072dd4c647cbfd01354162c3cc49d195a
jasmined09 <jasmined@anafore.com>
2021-09-08 23:14:05 +0800

    add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
 import React from 'react'
 import './App.css'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
 import NumbersToSum from './components/NumbersToSum'
 import SumToNumbers from './components/SumToNumbers'
 
 const App: React.FC = () => {
   return (
-    <div className="App">
+    <Router>
       <h1>KAKURO HELPER</h1>
-      <NumbersToSum />
-      <br />
-      <SumToNumbers />
-    </div>
+      <Switch>
+        <Route path="/numbers-to-sum">
+          <NumbersToSum />
+        </Route>
+        <Route path="/sum-to-numbers">
+          <SumToNumbers />
+        </Route>
+      </Switch>
+    </Router>
   )
 }
 
