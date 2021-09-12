739049fe23a09b4a534883a7de25f467896373d3
jasmined09 <jasmined@anafore.com>
2021-09-12 17:21:48 +0800

    add header to Home

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import './Home.css'
 
 const Home = (): JSX.Element => {
   return (
     <div>
-      <p>This app is an attempt to help solve Kakuro problem faster.</p>
+      <h3>HOME</h3>
+      <p className="TextCenter">
+        This app is an attempt to help solve Kakuro problem faster.
+      </p>
     </div>
   )
 }
